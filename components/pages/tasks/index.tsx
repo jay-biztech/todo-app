1eb1868957088ee8f04b43f72362544dd022ad4c
Jay Parmar <jay.parmar@biztechcs.lan>
2023-05-18 12:31:24 +0530

    WIP: Delete task

--- components/pages/tasks/index.tsx
+++ components/pages/tasks/index.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import { useTasksQuery } from '../../../services/tasks';
+import Button, { ButtonType } from '../../atoms/Button';
 
 export const Tasks: React.FC = () => {
   const { data: tasks, isLoading } = useTasksQuery();
@@ -9,7 +11,18 @@ export const Tasks: React.FC = () => {
         {tasks === undefined || isLoading
           ? 'Loading...'
           : tasks.map((task) => {
-              return <li key={task.id}>{task.name}</li>;
+              return (
+                <li key={task.id}>
+                  <div className="d-flex justify-content-between">
+                    {task.name}
+                    <Button
+                      title="Delete"
+                      buttonType={ButtonType.Danger}
+                      onClick={() => alert(task.id)}
+                    />
+                  </div>
+                </li>
+              );
             })}
       </ul>
     </>
