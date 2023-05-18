2bf7a7ed1acc7452357643748195383518e304b6
Jay Parmar <jay.parmar@biztechcs.lan>
2023-05-18 10:05:18 +0530

    Url changes

--- services/tasks/index.ts
+++ services/tasks/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Task } from '../../pages/api/tasks/types';
 
 export const fetchTasks = async () => {
-  const { data } = await axios.get(`http://localhost:3000/api/tasks`);
+  const { data } = await axios.get('/api/tasks');
   return data;
 };
 
