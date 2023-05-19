18c7a74a4908392230b34094fba088466589af76
Jay Parmar <jay.parmar@biztechcs.lan>
2023-05-19 12:43:16 +0530

    Set config for query keys

--- services/tasks/useDeleteTaskMutation.ts
+++ services/tasks/useDeleteTaskMutation.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { useMutation, useQueryClient } from 'react-query';
 import { Task } from '../../pages/api/tasks/types';
+import { QUERY_KEYS } from '../config';
+
+const { TASKS_QUERY_KEY } = QUERY_KEYS;
 
 export const deleteTask = async (id: number) => {
   const { data } = await axios.delete(`/api/tasks/${id}`);
@@ -12,11 +15,11 @@ export const useDeleteTaskMutation = () => {
 
   return useMutation(deleteTask, {
     onMutate: async (id) => {
-      await queryClient.cancelQueries({ queryKey: ['tasks'] });
+      await queryClient.cancelQueries({ queryKey: [TASKS_QUERY_KEY] });
 
-      const previousValue = queryClient.getQueryData(['tasks']);
+      const previousValue = queryClient.getQueryData([TASKS_QUERY_KEY]);
 
-      queryClient.setQueryData<Task[]>(['tasks'], (old) =>
+      queryClient.setQueryData<Task[]>([TASKS_QUERY_KEY], (old) =>
         (old || []).filter((task) => task.id !== id)
       );
 
--- services/tasks/useTasks.ts
+++ services/tasks/useTasks.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
-import { Task } from '../../pages/api/tasks/types';
 import { useQuery } from 'react-query';
+import { Task } from '../../pages/api/tasks/types';
+import { QUERY_KEYS } from '../config';
+
+const { TASKS_QUERY_KEY } = QUERY_KEYS;
 
 export const fetchTasks = async () => {
   const { data } = await axios.get('/api/tasks');
   return data;
 };
 
-export const useTasks = () => useQuery<Task[]>(['tasks'], () => fetchTasks());
+export const useTasks = () =>
+  useQuery<Task[]>([TASKS_QUERY_KEY], () => fetchTasks());
