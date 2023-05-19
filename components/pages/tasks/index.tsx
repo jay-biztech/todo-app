b957947d125cda49771db58eac03e0dae6fc54a9
Jay Parmar <jay.parmar@biztechcs.lan>
2023-05-19 12:32:19 +0530

    Add: utils

--- components/pages/tasks/index.tsx
+++ components/pages/tasks/index.tsx
@@ -1,16 +1,11 @@
 import { useTasks } from '../../../services/tasks';
 import { Task } from '../../organisms/task';
+import { completed, inProgress } from './utils';
 
 export const Tasks: React.FC = () => {
   const { data: tasks, isLoading } = useTasks();
-
-  const inProgressTasks = () => {
-    return tasks?.filter((task) => !task.isCompleted);
-  };
-
-  const completedTasks = () => {
-    return tasks?.filter((task) => task.isCompleted);
-  };
+  const inProgressTasks = inProgress(tasks);
+  const completedTasks = completed(tasks);
 
   return (
     <>
@@ -20,14 +15,14 @@ export const Tasks: React.FC = () => {
         <>
           <ul>
             <h4>In progress</h4>
-            {inProgressTasks()?.map(({ id, name }) => {
+            {inProgressTasks?.map(({ id, name }) => {
               return <Task key={id} {...{ id, name }} />;
             })}
           </ul>
 
           <ul>
             <h4>Completed</h4>
-            {completedTasks()?.map(({ id, name }) => {
+            {completedTasks?.map(({ id, name }) => {
               return <Task key={id} {...{ id, name }} />;
             })}
           </ul>
