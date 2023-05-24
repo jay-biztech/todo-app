c0e47f777df2eac3d1751ba52c4263e6da187e84
Jay Parmar <jay.parmar@biztechcs.lan>
2023-05-24 19:23:52 +0530

    WIP: Due date

--- pages/tasks/create.tsx
+++ pages/tasks/create.tsx
@@ -1,22 +1,27 @@
 import { Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 import Button from '../../components/atoms/Button';
 import { ButtonType } from '../../components/atoms/Button/types';
 import { Heading } from '../../components/atoms/Heading';
 import { useCreateTaskMutation } from '../../services/tasks';
+import { useState } from 'react';
 
 const CreateTask: React.FC = () => {
-  const { mutate: createTask } = useCreateTaskMutation();
-
   const TaskSchema = Yup.object().shape({
     name: Yup.string()
       .min(2, 'Too Short!')
       .max(50, 'Too Long!')
       .required('Title is required'),
     isCompleted: Yup.boolean().default(false),
-    date: Yup.date(),
+    dueDate: Yup.date().required('Date is required'),
   });
+
+  const { mutate: createTask } = useCreateTaskMutation();
+  const [dueDate, setDueDate] = useState<Date>();
 
   return (
     <>
@@ -27,10 +32,12 @@ const CreateTask: React.FC = () => {
         initialValues={{
           name: '',
           isCompleted: false,
-          date: new Date(),
+          dueDate: null,
         }}
         validationSchema={TaskSchema}
         onSubmit={(task, actions) => {
+          console.log(task);
+
           createTask(task);
           actions.setSubmitting(false);
           actions.resetForm();
@@ -47,6 +54,21 @@ const CreateTask: React.FC = () => {
                 className="form-control"
               />
               {errors.name && touched.name ? <div>{errors.name}</div> : null}
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="date">Date</label>
+              <DatePicker
+                id="dueDate"
+                name="dueDate"
+                selected={dueDate}
+                onChange={(date: Date) => setDueDate(date)}
+                className="form-control"
+                value={dueDate}
+              />
+              {errors.dueDate && touched.dueDate ? (
+                <div>{errors.dueDate}</div>
+              ) : null}
             </div>
 
             <Button
